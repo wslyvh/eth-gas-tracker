b6f736151bdd150f77305b5164ce6d3918a36c2d
wslyvh <wslyvh@users.noreply.github.com>
2024-03-04 21:49:10 +0200

    remove metatags

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,17 +6,6 @@ import {
 } from "@tanstack/react-query";
 import { LatestDataWrapper } from "./components/latest/latest";
 import { HistoryDataWrapper } from "./components/history";
-import { getFrameMetadata } from "frog/next";
-import type { Metadata } from "next";
-
-export async function generateMetadata(): Promise<Metadata> {
-  const frameTags = await getFrameMetadata(
-    `${process.env.VERCEL_URL || "http://localhost:3000"}/api`
-  );
-  return {
-    other: frameTags,
-  };
-}
 
 export default async function Home() {
   const queryClient = new QueryClient();
