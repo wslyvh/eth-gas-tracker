6263ac1e5765d8040959ee6ab34c942b85b20211
wslyvh <wslyvh@users.noreply.github.com>
2024-08-03 13:16:33 +0200

    add sqlite

--- src/app/api/gas/average/route.ts
+++ src/app/api/gas/average/route.ts
@@ -1,3 +1,4 @@
+import { createClient as sqlite } from "@libsql/client";
 import { createClient } from "@supabase/supabase-js";
 import { NextResponse } from "next/server";
 
@@ -20,6 +21,17 @@ export async function GET() {
     const network = "mainnet";
     // limit: 24 | 168
     const limit = 168;
+
+    try {
+      console.log("Fetching Sqlite data...");
+      const db1 = sqlite({
+        url: `file:src/db/${network}.db`,
+      });
+      const { rows: blocks } = await db1.execute("SELECT * FROM blocks");
+      console.log("BLOCKS", blocks);
+    } catch (e) {
+      console.error("Error:", e);
+    }
 
     const { data, error } = await db
       .from(`gasdata_${network}_${period}`)
