2159b4662afc8b9ee4a443ff06402e212908df6e
wslyvh <wslyvh@users.noreply.github.com>
2024-12-14 18:38:00 +0100

    ethprice

--- src/utils/gas.ts
+++ src/utils/gas.ts
@@ -1,45 +1,46 @@
-import { parseAbi } from "viem/utils";
 import { CreatePublicClient } from "./web3";
 import { NETWORKS } from "./config";
 import { getAverage, toRoundedGwei } from "./math";
 import { GetBlockParameters } from "viem/actions";
+import { getEthPrice } from "@/services/ether";
 
 export async function getGasData(network: NETWORKS, blockNr?: number) {
   const client = CreatePublicClient(network);
   const mainnet = CreatePublicClient("mainnet");
 
-  const blockParams: GetBlockParameters = blockNr ? { blockNumber: BigInt(blockNr) } : { blockTag: "latest" };
-  const pendingParams: GetBlockParameters = blockNr ? { blockNumber: BigInt(blockNr + 1) } : { blockTag: "pending" };
-  let feeHistoryParams: any = { blockCount: 25, rewardPercentiles: [1, 40, 80] };
+  const blockParams: GetBlockParameters = blockNr
+    ? { blockNumber: BigInt(blockNr) }
+    : { blockTag: "latest" };
+  const pendingParams: GetBlockParameters = blockNr
+    ? { blockNumber: BigInt(blockNr + 1) }
+    : { blockTag: "pending" };
+  let feeHistoryParams: any = {
+    blockCount: 25,
+    rewardPercentiles: [1, 40, 80],
+  };
   if (blockNr) feeHistoryParams.blockNumber = BigInt(blockNr);
   if (!blockNr) feeHistoryParams.blockTag = "pending";
- 
+
   try {
     const [block, pending, history, ethPrice] = await Promise.all([
       await client.getBlock(blockParams),
       await client.getBlock(pendingParams),
       await client.getFeeHistory(feeHistoryParams),
-      mainnet
-        .readContract({
-          address: "0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419", // Chainlink Price Feed
-          abi: parseAbi(["function latestAnswer() view returns (int256)"]),
-          functionName: "latestAnswer",
-        })
-        .then((res) => Math.round((Number(res) / 1e8) * 100) / 100),
+      await getEthPrice(),
     ]);
 
     if (!block.baseFeePerGas || !pending.baseFeePerGas) {
-      return 'No baseFee found (pre-1559 block).';
+      return "No baseFee found (pre-1559 block).";
     }
 
     const fees = formatFeeHistory(history);
-    const slow = getAverage(fees.map((b) => b.priorityFeePerGas[0]))
-    const normal = getAverage(fees.map((b) => b.priorityFeePerGas[1]))
-    const fast = getAverage(fees.map((b) => b.priorityFeePerGas[2]))
+    const slow = getAverage(fees.map((b) => b.priorityFeePerGas[0]));
+    const normal = getAverage(fees.map((b) => b.priorityFeePerGas[1]));
+    const fast = getAverage(fees.map((b) => b.priorityFeePerGas[2]));
 
-    const baseFee = toRoundedGwei(block.baseFeePerGas)
-    const nextFee = toRoundedGwei(pending.baseFeePerGas)
-      
+    const baseFee = toRoundedGwei(block.baseFeePerGas);
+    const nextFee = toRoundedGwei(pending.baseFeePerGas);
+
     return {
       network: network,
       blockNr: String(block.number),
@@ -51,7 +52,9 @@ export async function getGasData(network: NETWORKS, blockNr?: number) {
       block: {
         gasLimit: Number(block.gasLimit),
         gasUsed: Number(block.gasUsed),
-        utilization: Math.round((Number(block.gasUsed) / Number(block.gasLimit)) * 100),
+        utilization: Math.round(
+          (Number(block.gasUsed) / Number(block.gasLimit)) * 100
+        ),
         transactionCount: block.transactions.length,
       },
       oracle: {
@@ -63,7 +66,9 @@ export async function getGasData(network: NETWORKS, blockNr?: number) {
         normal: {
           gasFee: Math.round(normal + Number(pending.baseFeePerGas)),
           priorityFee: Math.round(normal),
-          gwei: toRoundedGwei(Math.round(normal + Number(pending.baseFeePerGas))),
+          gwei: toRoundedGwei(
+            Math.round(normal + Number(pending.baseFeePerGas))
+          ),
         },
         fast: {
           gasFee: Math.round(fast + Number(pending.baseFeePerGas)),
@@ -85,7 +90,9 @@ export function formatFeeHistory(result: any, inclPending = false) {
       number: result.baseFeePerGas[i],
       baseFeePerGas: Number(result.baseFeePerGas[i]),
       gasUsedRatio: Number(result.gasUsedRatio[i]),
-      priorityFeePerGas: result.reward[i].map((x: any) => Number(x)) as number[],
+      priorityFeePerGas: result.reward[i].map((x: any) =>
+        Number(x)
+      ) as number[],
     });
   }
 
