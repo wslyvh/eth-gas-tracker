6915549db17f3dff8209c9892400cc5e2aa391fe
wslyvh <wslyvh@users.noreply.github.com>
2025-01-11 16:56:58 +0100

    query defaults

--- src/app/providers/DataProvider.tsx
+++ src/app/providers/DataProvider.tsx
@@ -7,8 +7,12 @@ function makeQueryClient() {
   return new QueryClient({
     defaultOptions: {
       queries: {
-        // With SSR, keep above 0 to avoid refetching immediately on the client
         staleTime: 12 * 1000,
+        gcTime: 24 * 1000,
+        refetchOnWindowFocus: false,
+        refetchOnReconnect: false,
+        retryOnMount: false,
+        retry: false,
       },
     },
   });
@@ -18,10 +22,8 @@ let browserQueryClient: QueryClient | undefined = undefined;
 
 function getQueryClient() {
   if (typeof window === "undefined") {
-    // Server: always make a new query client
     return makeQueryClient();
   } else {
-    // Browser: make a new query client if we don't already have one
     if (!browserQueryClient) browserQueryClient = makeQueryClient();
     return browserQueryClient;
   }
