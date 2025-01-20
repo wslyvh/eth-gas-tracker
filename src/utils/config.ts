0d1dab9438a59e76c757176b2161df53df734f6d
wslyvh <wslyvh@users.noreply.github.com>
2025-01-20 21:51:58 +0100

    alchemy endpoint

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -13,11 +13,15 @@ export const CONFIG = {
   NODE_ENV: process.env.NODE_ENV || "development",
 
   INFURA_API_KEY: process.env.INFURA_API_KEY ?? "",
+  ALCHEMY_MAIN_API_KEY: process.env.ALCHEMY_MAIN_API_KEY ?? "",
   ALCHEMY_API_KEY: process.env.ALCHEMY_API_KEY ?? "",
 };
 (() => {
   if (!process.env.INFURA_API_KEY) {
     console.error("You need to provide a INFURA_API_KEY env variable");
+  }
+  if (!process.env.ALCHEMY_MAIN_API_KEY) {
+    console.error("You need to provide a ALCHEMY_MAIN_API_KEY env variable");
   }
   if (!process.env.ALCHEMY_API_KEY) {
     console.error("You need to provide a ALCHEMY_API_KEY env variable");
--- src/utils/web3.ts
+++ src/utils/web3.ts
@@ -1,33 +1,38 @@
-import { Chain, createPublicClient, http } from "viem"
-import { arbitrum, mainnet, optimism } from "viem/chains"
-import { CONFIG, NETWORKS } from "./config"
+import { Chain, createPublicClient, http } from "viem";
+import { arbitrum, mainnet, optimism } from "viem/chains";
+import { CONFIG, NETWORKS } from "./config";
 
-export const NETWORKS_CHAIN = ["mainnet", "arbitrum", "base", "optimism"]
+export const NETWORKS_CHAIN = ["mainnet", "arbitrum", "base", "optimism"];
 
 export function CreatePublicClient(network: NETWORKS = "mainnet") {
-    let chain: Chain = mainnet
-    let url = `https://mainnet.infura.io/v3/${CONFIG.INFURA_API_KEY}`
+  let chain: Chain = mainnet;
 
-    if (network === "arbitrum") {
-        chain = arbitrum
-        url = `https://arb-mainnet.g.alchemy.com/v2/${CONFIG.ALCHEMY_API_KEY}`
-    }
+  let url = `https://eth-mainnet.g.alchemy.com/v2/${CONFIG.ALCHEMY_MAIN_API_KEY}`;
+  // let url =
+  //   new Date().getHours() % 2 === 0
+  //     ? `https://eth-mainnet.g.alchemy.com/v2/${CONFIG.ALCHEMY_MAIN_API_KEY}`
+  //     : `https://mainnet.infura.io/v3/${CONFIG.INFURA_API_KEY}`;
 
-    if (network === "base") {
-        chain = arbitrum
-        url = `https://mainnet.base.org`
-    }
+  if (network === "arbitrum") {
+    chain = arbitrum;
+    url = `https://arb-mainnet.g.alchemy.com/v2/${CONFIG.ALCHEMY_API_KEY}`;
+  }
 
-    if (network === "optimism") {
-        chain = optimism
-        url = `https://optimism.publicnode.com`
-    }
+  if (network === "base") {
+    chain = arbitrum;
+    url = `https://mainnet.base.org`;
+  }
 
-    return createPublicClient({
-        chain: chain,
-        batch: {
-            multicall: true,
-        },
-        transport: http(url),
-    })
+  if (network === "optimism") {
+    chain = optimism;
+    url = `https://optimism.publicnode.com`;
+  }
+
+  return createPublicClient({
+    chain: chain,
+    batch: {
+      multicall: true,
+    },
+    transport: http(url),
+  });
 }
